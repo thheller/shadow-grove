17f59361db69bd81cbfcd4c0eab7612d8fb4e6bd
Thomas Heller <th.heller@gmail.com>
2020-09-10 13:29:21 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
-(defproject thheller/shadow-experiments "0.0.1"
+(defproject thheller/shadow-experiments "0.0.2"
   :description "WARNING: Experimental Code! Changing completely without notice! Do not use for anything but other experiments!"
   :url "http://github.com/thheller/shadow-experiments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false}}
+
   :dependencies
-  [[org.clojure/clojure "1.10.0" :scope "provided"]
-   [org.clojure/clojurescript "1.10.597" :scope "provided"]]
+  [[org.clojure/clojure "1.10.1" :scope "provided"]
+   [org.clojure/clojurescript "1.10.773" :scope "provided"]]
 
   :resource-paths
   ["src/resources"]
@@ -22,5 +26,5 @@
    {:source-paths ["src/dev"]}
    :cljs-dev ;; using this in shadow-cljs so can't use dev profile (Cursive complains about circular dep)
    {:dependencies
-    [[thheller/shadow-cljs "2.8.80"]]}})
+    [[thheller/shadow-cljs "2.11.2"]]}})
 
