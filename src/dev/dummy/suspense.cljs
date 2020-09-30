57b3377c8248fd70e27462ab81b1acb95b93993c
Thomas Heller <th.heller@gmail.com>
2020-09-30 10:53:41 +0200

    beef up suspense example a bit

--- src/dev/dummy/suspense.cljs
+++ src/dev/dummy/suspense.cljs
@@ -6,10 +6,10 @@
 (defonce root-el (js/document.getElementById "app"))
 
 (defc nested [x]
-  (bind _ (t/rand-delay 1000))
+  (bind _ (t/rand-delay 250))
 
   (render
-    (<< [:div.border.shadow.m-4.p-4 "nested:" x])))
+    (<< [:div.border.shadow.my-4.p-4 "nested:" x])))
 
 (defn content []
   (<< (nested 1)
@@ -18,23 +18,58 @@
       (nested 4)
       (nested 5)))
 
-(defc ui-root []
-  (render
-    (<< [:div.flex {:style "width: 500px;"}
-         [:div.flex-1
-          [:div.pl-4 "with suspense"]
-          (sg/suspense
-            {:fallback (<< [:div.pl-4 "Loading ..."])
-             :timeout 500}
-            (content))]
+(defn page-a []
+  (<< [:div "page a"]
+      (content)))
 
-         [:div.flex-1
-          [:div.pl-4 "without"]
-          (content)]])))
+(defn page-b []
+  (<< [:div "page b"]
+      (content)))
+
+(defn page-c []
+  (<< [:div "page c"]
+      (content)))
+
+(defc ui-root []
+  (bind {:keys [page]}
+    (sg/env-watch :data-ref))
+
+  (event ::switch-page! [{:keys [data-ref] :as env} which e]
+    (swap! data-ref assoc :page which))
+
+  (render
+    (let [body
+          (case page
+            :a (page-a)
+            :b (page-b)
+            :c (page-c))]
+
+      (<< [:div.p-4
+           [:a.underline {:href "#" :on-click [::switch-page! :a]} "page a"]
+           " "
+           [:a.underline {:href "#" :on-click [::switch-page! :b]} "page b"]
+           " "
+           [:a.underline {:href "#" :on-click [::switch-page! :c]} "page c"]]
+
+          [:div.flex {:style "width: 500px;"}
+           [:div.flex-1.p-4
+            [:div "with suspense"]
+            (sg/suspense
+              {:fallback (<< [:div.pl-4 "Loading ..."])
+               :timeout 500}
+              body)]
+
+           [:div.flex-1.p-4
+            [:div "without"]
+            body]]))))
 
 (defn ^:dev/after-load start []
   (sg/start ::ui root-el (ui-root)))
 
+(defonce data-ref (atom {:page :a}))
+
 (defn init []
-  (sg/init ::ui {} [])
+  (sg/init ::ui
+    {:data-ref data-ref}
+    [])
   (start))
