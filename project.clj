83b7a32197121f83553d2a666d58c4b90e3521bc
Thomas Heller <th.heller@gmail.com>
2021-03-02 16:24:28 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-experiments "0.0.3"
+(defproject thheller/shadow-experiments "0.0.4"
   :description "WARNING: Experimental Code! Changing completely without notice! Do not use for anything but other experiments!"
   :url "http://github.com/thheller/shadow-experiments"
   :license {:name "Eclipse Public License"
@@ -10,10 +10,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.10.1" :scope "provided"]
-   [org.clojure/clojurescript "1.10.773" :scope "provided"]
-   [metosin/reitit-core "0.5.12"]
-   [thheller/shadow-undertow "0.1.0"]
-   [hiccup "1.0.5"]]
+   [org.clojure/clojurescript "1.10.773" :scope "provided"]]
 
   :resource-paths
   ["src/resources"]
@@ -26,8 +23,13 @@
 
   :profiles
   {:dev
-   {:source-paths ["src/dev"]}
+   {:source-paths ["src/dev"]
+    :dependencies
+    [[hiccup "1.0.5"]
+     [metosin/reitit-core "0.5.12"]
+     [thheller/shadow-undertow "0.1.0"]]}
    :cljs-dev ;; using this in shadow-cljs so can't use dev profile (Cursive complains about circular dep)
    {:dependencies
-    [[thheller/shadow-cljs "2.11.7"]]}})
+    [[thheller/shadow-cljs "2.11.19"]
+     ]}})
 
