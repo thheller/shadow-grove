0c4b7e7f50af362fc86aa2c00d64bbe3148064a4
Thomas Heller <th.heller@gmail.com>
2021-03-30 23:43:27 +0200

    rewrite history module
    get rid of closure lib impl, more complicated to use
    that instead of just doing everything directly.

--- src/main/shadow/experiments/grove/history.cljs
+++ src/main/shadow/experiments/grove/history.cljs
@@ -4,24 +4,45 @@
     [shadow.experiments.grove :as sg]
     [shadow.experiments.grove.runtime :as rt]
     [shadow.experiments.grove.events :as ev]
-    [shadow.experiments.arborist.attributes :as attr])
-  (:import [goog.history Html5History]))
+    [shadow.experiments.arborist.attributes :as attr]))
 
 (defn init!
   [rt-ref
    {:keys [start-token path-prefix use-fragment]
     :or {start-token "/dashboard"
-         path-prefix "/"
+         path-prefix ""
          use-fragment false}
     :as config}]
 
-  {:pre [(str/starts-with? path-prefix "/")
-         (not (str/ends-with? path-prefix "/"))]}
+  {:pre [(or (= "" path-prefix)
+             (and (string? path-prefix)
+                  (str/starts-with? path-prefix "/")
+                  (not (str/ends-with? path-prefix "/")))
 
-  (let [history
-        (doto (Html5History.)
-          (.setPathPrefix path-prefix)
-          (.setUseFragment use-fragment))]
+             (and (str/starts-with? start-token "/")
+                  (not (str/ends-with? start-token "/")))
+             )]}
+
+  (let [get-token
+        (fn []
+          (if-not use-fragment
+            (let [path js/window.location.pathname]
+              (cond
+                (= path path-prefix)
+                "/"
+
+                (str/starts-with? path path-prefix)
+                (subs path (count path-prefix))
+
+                :else
+                (throw (ex-info "path did not match path prefix" {:path path :path-prefix path-prefix}))
+                ))
+            (let [hash js/window.location.hash]
+              ;; always start everything with a / even when using hash
+              ;; is "" when url doesn't have a hash, otherwise #foo
+              (if (= hash "")
+                "/"
+                (subs js/window.location.hash (+ 1 (count path-prefix)))))))]
 
     (attr/add-attr :ui/href
       (fn [env node oval nval]
@@ -32,51 +53,72 @@
 
           (set! node -href
             (if use-fragment
-              (str "#" nval)
+              (str "#" path-prefix nval)
               (str path-prefix
                    (if-not (str/ends-with? path-prefix "/")
                      nval
                      (subs nval 1))))))))
 
     (ev/reg-fx rt-ref :ui/redirect!
-      (fn [env token]
+      (fn [env {:keys [token title]}]
         {:pre [(str/starts-with? token "/")]}
-        ;; FIXME: should accept map to build token
-        (.setToken history token)))
+        (js/window.history.pushState
+          nil
+          (or title js/document.title)
+          (str path-prefix token))))
 
     (swap! rt-ref
       (fn [rt]
         (-> rt
-            (assoc ::history history ::config config)
+            (assoc ::config config)
             (update ::rt/env-init conj
               (fn [{::rt/keys [root-el] :as env}]
 
-                (let [first-token (.getToken history)]
-                  (when (and (= "" first-token) (seq start-token))
-                    (.replaceToken history start-token)))
+                (let [trigger-route!
+                      (fn []
+                        ;; token must start with /, strip it to get tokens vector
+                        (let [token (get-token)
+                              tokens (str/split (subs token 1) #"/")]
 
-                (.listen history js/goog.history.EventType.NAVIGATE
-                  (fn [^goog e]
-                    ;; token must start with /, strip it to get tokens vector
-                    (let [token (.-token e)
-                          tokens (str/split (subs token 1) #"/")]
+                          (sg/run-now! env #(sg/run-tx env {:e :ui/route! :token token :tokens tokens}))))
 
-                      (sg/run-now! env #(sg/run-tx env {:e :ui/route! :token token :tokens tokens})))))
+                      first-token
+                      (get-token)]
 
-                ;; fragment uses hashchange event so we can skip checking clicks
-                (when-not use-fragment
-                  (.addEventListener root-el "click"
-                    (fn [^js e]
-                      (when (and (zero? (.-button e))
-                                 (not (or (.-shiftKey e) (.-metaKey e) (.-ctrlKey e) (.-altKey e))))
-                        (when-let [a (some-> e .-target (.closest "a"))]
+                  ;; fragment uses hashchange event so we can skip checking clicks
+                  (when-not use-fragment
+                    (.addEventListener root-el "click"
+                      (fn [^js e]
+                        (when (and (zero? (.-button e))
+                                   (not (or (.-shiftKey e) (.-metaKey e) (.-ctrlKey e) (.-altKey e))))
+                          (when-let [a (some-> e .-target (.closest "a"))]
 
-                          (let [href (.getAttribute a "href")
-                                a-target (.getAttribute a "target")]
+                            (let [href (.getAttribute a "href")
+                                  a-target (.getAttribute a "target")]
 
-                            (when (and href (seq href) (str/starts-with? href path-prefix) (nil? a-target))
-                              (.preventDefault e)
-                              (.setToken history (subs href (count path-prefix))))))))))
+                              (when (and href (seq href) (str/starts-with? href path-prefix) (nil? a-target))
+                                (.preventDefault e)
 
-                (.setEnabled history true)
+                                (js/window.history.pushState nil js/document.title href)
+
+                                (trigger-route!)
+                                )))))))
+
+                  (when (and (= "/" first-token) (seq start-token))
+                    (js/window.history.replaceState
+                      nil
+                      js/document.title
+                      (str (when use-fragment "#") path-prefix start-token)))
+
+                  (trigger-route!)
+
+                  (js/window.addEventListener "popstate"
+                    (fn [e]
+                      (trigger-route!)))
+
+                  (when use-fragment
+                    (js/window.addEventListener "hashchange"
+                      (fn [e]
+                        (trigger-route!)))))
+
                 env)))))))
