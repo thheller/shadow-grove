062e3478df46190e6867b839847c857f12a13322
Thomas Heller <th.heller@gmail.com>
2024-02-06 19:06:51 +0100

    writing proper tests is hard

--- src/test/shadow/arborist/fragments_test.cljs
+++ src/test/shadow/arborist/fragments_test.cljs
@@ -1,12 +1,39 @@
 (ns shadow.arborist.fragments-test
   (:require
     ["jsdom" :refer (JSDOM)]
-    [cljs.test :as ct :refer (deftest is)]
+    [cljs.test :as ct :refer (deftest is async)]
+    [shadow.cljs.modern :refer (js-await)]
     [shadow.arborist.attributes :as sa]
-    [shadow.grove :as sg :refer (<<)]
-    [shadow.grove.db :as db]))
+    [shadow.grove :as sg :refer (defc <<)]
+    [shadow.grove.db :as db]
+    [shadow.grove.runtime :as rt]))
 
-(deftest test-jsdom
+
+;; how the hell does anyone debug exceptions thrown in deftest
+
+(defmethod ct/report [::ct/default :error] [{:keys [actual] :as m}]
+  ;; gimme the stack already, just the message is not enough
+  (println (.-stack actual))
+  (ct/inc-report-counter! :error)
+  (println "\nERROR in" (ct/testing-vars-str m))
+  (when (seq (:testing-contexts (ct/get-current-env)))
+    (println (ct/testing-contexts-str)))
+  (when-let [message (:message m)] (println message))
+  (ct/print-comparison m))
+
+(set! cljs.core/missing-protocol
+  (fn missing-protocol [proto obj]
+    ;; tap, since "No protocol method ... for type [object Object]" is not exactly helpful.
+    (tap> [:missing-protocol obj proto])
+    (let [ty (type obj)
+          ty (if (and ty (.-cljs$lang$type ty))
+               (.-cljs$lang$ctorStr ty)
+               (goog/typeOf obj))]
+      (js/Error.
+        (.join (array "No protocol method " proto
+                 " defined for type " ty ": " obj) "")))))
+
+(deftest test-basic-fragment-ops
   (let [dom (JSDOM. "<!DOCTYPE html>")
         doc (-> dom .-window .-document)
         root (.-body doc)
@@ -51,6 +78,77 @@
       (is (= nil (:nval @rt-ref)))
       (is (= "bar" (:oval @rt-ref)))
       )))
+
+(defc ui-dummy [state-ref order-ref]
+  (hook (swap! order-ref conj :slot-1))
+  (bind x (sg/watch state-ref))
+  (bind y
+    (swap! order-ref conj :slot-y)
+    (str x "y"))
+  (render y))
+
+
+(deftest test-component-lifecycle
+  (let [dom (JSDOM. "<!DOCTYPE html>")
+        doc (-> dom .-window .-document)
+        root (.-body doc)
+
+        data-ref
+        (-> {}
+            (db/configure {})
+            (atom))
+
+        rt-ref
+        (-> {}
+            (sg/prepare data-ref :test))]
+
+    ;; hack until I decide whether document should come from env?
+    ;; used in a few places and currently always js/document
+    (set! js/global -document doc)
+
+    (let [state-ref (atom "foo")
+          order-ref (atom [])]
+
+      (sg/render rt-ref root (ui-dummy state-ref order-ref))
+      (is (= [:slot-1 :slot-y] @order-ref))
+
+      ;; not using innerHTML since dev builds leave a component boundary comment
+      (is (= "fooy" (.-textContent root)))
+
+      ;; dom update is scheduled for next-tick to batch updates
+      (reset! state-ref "bar")
+
+      (async done
+        (js-await [_ rt/ticker]
+          ;; first slot untouched, does not run again
+          (is (= [:slot-1 :slot-y :slot-y] @order-ref))
+          (is (= "bary" (.-textContent root)))
+
+          (let [before @order-ref]
+
+            ;; render component again from root, same args
+            (sg/render rt-ref root (ui-dummy state-ref order-ref))
+
+            (is (= "bary" (.-textContent root)))
+
+            ;; slots should not have run again
+            (is (= before @order-ref))
+
+            (let [test-ref (atom [])
+                  before @order-ref]
+
+              (sg/render rt-ref root (ui-dummy state-ref test-ref))
+
+              ;; prev arg untouched
+              (is (= before @order-ref))
+
+              ;; since new arg, also running y again
+              (is (= [:slot-1 :slot-y] @test-ref))
+
+              (done)))))
+      )))
+
+
 
 (comment
   (test-jsdom))
